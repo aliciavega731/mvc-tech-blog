1aad3ccf8915ab1811e4b88e7f56bd8eba366987
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-03-21 17:44:06 -0700

    updated without vote

--- models/index.js
+++ models/index.js
@@ -13,38 +13,28 @@ Post.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
+// User.belongsToMany(Post, {
+//   through: Vote,
+//   as: 'voted_posts',
 
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
+//   foreignKey: 'user_id',
+//   onDelete: 'SET NULL'
+// });
 
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
+// Post.belongsToMany(User, {
+//   through: Vote,
+//   as: 'voted_posts',
+//   foreignKey: 'post_id',
+//   onDelete: 'SET NULL'
+// });
 
-Vote.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
+// User.hasMany(Vote, {
+//   foreignKey: 'user_id'
+// });
 
-Vote.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
-});
+// Post.hasMany(Vote, {
+//   foreignKey: 'post_id'
+// });
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
@@ -65,4 +55,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
